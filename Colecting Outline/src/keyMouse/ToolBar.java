9d462ab626ede7242c16efcdb1b5616df22b5e9e
jeronangell <elder.angell@gmail.com>
2017-08-16 06:54:33 -0600

    master1

---
+++ Colecting Outline/src/keyMouse/ToolBar.java
@@ -0,0 +1,14 @@
+package keyMouse;
+
+import java.awt.Point;
+
+public class ToolBar {
+
+	public Point pnt;
+public void findItems(){
+	int y=pnt.y+18;
+	for(int x=pnt.x+19;x<pnt.x+390;x+=40){
+		
+	}
+}
+}
